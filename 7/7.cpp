10f96cd75c388e77a0efaa9edea83b6820cee717
yoyis <yoyis@roxdeb.mail>
2025-01-21 22:37:54 +0100

    Advent23-7: Problems with vector, changed to arr. Need to do good comparator method

--- 7/7.cpp
+++ 7/7.cpp
@@ -1,4 +1,90 @@
+#include <array>
+#include <algorithm>
+
 #include "../adventbasics.h"
+#include "7.h"
+
+Poker_Tuple::Poker_Tuple(std::string& tuple){
+    tuple.copy(hand, 5, 0);
+    bid = std::stoi(tuple.substr(6));
+}
+
+Poker_Tuple::Poker_Tuple(Poker_Tuple& t):bid{t.bid}{
+    for(int i = 0; i < 5; i++)
+        hand[i] = t.hand[i];
+}
+
+int nCharsOfA(char hand[5], char c){
+    int n = 0;
+
+    for(int i = 0; i < 5; i++){
+        if(hand[i] == c)
+            n++;
+    }
+
+    return n;
+}
+
+//TODO: My dumbass did this. Fix doing one method that return an enum.
+bool hasRepoker(Poker_Tuple& t){
+    for(int i = 0; i < 13; i++){
+       if(nCharsOfA(t.hand, CARDS[i]) == 5)
+           return true;
+    }
+    return false;
+}
+bool hasPoker(Poker_Tuple& t){
+    for(int i = 0; i < 13; i++){
+       if(nCharsOfA(t.hand, CARDS[i]) == 4)
+           return true;
+    }
+    return false;
+}
+bool hasFull(Poker_Tuple& t){
+    return hasThree(t) && hasTwo(t);
+}
+bool hasThree(Poker_Tuple& t){
+    for(int i = 0; i < 13; i++){
+       if(nCharsOfA(t.hand, CARDS[i]) == 3)
+           return true;
+    }return false;
+}
+bool hasDoubleTwo(Poker_Tuple& t){
+    bool firstHit = true;
+
+    for(int i = 0; i < 13; i++){
+        if(nCharsOfA(t.hand, CARDS[i]) == 2){
+           if(firstHit)
+               firstHit = false;
+           else
+               return true;
+        }
+    }
+
+    return false;
+}
+bool hasTwo(Poker_Tuple& t){
+    for(int i = 0; i < 13; i++){
+       if(nCharsOfA(t.hand, CARDS[i]) == 2)
+           return true;
+    }return false;
+}
+
+bool handComparator(Poker_Tuple* t1, Poker_Tuple* t2){
+    int i = 0;
+
+    while(i < 5){
+        if((t1->hand)[i] == (t2->hand)[i])
+            i++;
+        else if((t1->hand)[i] < (t2->hand)[i])
+            return true;
+        else
+            return false;
+    }
+
+    std::cout << "SHIT. Same hands. Returning true" << std::endl;
+    return true;
+}
 
 int main(int argc, char *argv[]){
     using namespace std;
@@ -14,6 +100,51 @@ int main(int argc, char *argv[]){
     adbasic::textToStrarr(in, lines, n_lines, '\n');
     cout << "Input proccessed to string arr" << endl;
 
-    for(int i = 0; i < n_lines; i++)
-        cout << lines[i] << endl;
+    //Algorithm
+    //TODO: Create an fixed size array of struct pointers to Poker_Tuple
+    // then, populate the arrays and use sort() function to do the
+    // sorting
+
+    array<Poker_Tuple*, 1000> aRepoker;
+    array<Poker_Tuple*, 1000> aPoker;
+    array<Poker_Tuple*, 1000> aFull;
+    array<Poker_Tuple*, 1000> aThree;
+    array<Poker_Tuple*, 1000> aDoubleTwo;
+    array<Poker_Tuple*, 1000> aTwo;
+    array<Poker_Tuple*, 1000> aOne;
+
+    int iR=0, iP=0, iF=0, iTh=0, iDTw=0, iTw=0, iO=0;
+
+    cout << "Populating arrays..." << endl;
+    for(int i = 0; i < n_lines; i++){
+        Poker_Tuple t(lines[i]);
+
+        if(hasRepoker(t))
+            aRepoker[iR++] = new Poker_Tuple(t);
+        else if(hasPoker(t))
+            aPoker[iP++] = new Poker_Tuple(t);
+        else if(hasFull(t))
+            aFull[iF++] = new Poker_Tuple(t);
+    }
+
+    cout << "Sorting..." << endl;
+    if(iR > 0)
+        sort(aRepoker.begin(), aRepoker.begin()+iR, handComparator);
+    if(iP > 0)
+        sort(aPoker.begin(), aPoker.begin()+iP, handComparator);
+    if(iF > 0)
+        sort(aFull.begin(), aFull.begin()+iF, handComparator);
+
+    cout << "\nRepoker:" << endl;
+    for(int i = 0; i < iR; i++){
+        cout << (*(aRepoker[i])).hand << endl;
+    }
+    cout << "\nPoker" << endl;
+    for(int i = 0; i < iP; i++){
+        cout << (*(aPoker[i])).hand << endl;
+    }
+    cout << "\nFull" << endl;
+    for(int i = 0; i < iP; i++){
+        cout << (*(aFull[i])).hand << endl;
+    }
 }
--- 7/7.h
+++ 7/7.h
@@ -1,14 +1,21 @@
+#include <string>
 const char CARDS[13] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
 
 struct Poker_Tuple{
     char hand[5];
     int bid;
 
-    Poker_Tuple(char h[5], int b);
+    Poker_Tuple(std::string& tuple);
+    Poker_Tuple(Poker_Tuple& t);
 };
 
 bool hasRepoker(Poker_Tuple&);
 bool hasPoker(Poker_Tuple&);
 bool hasFull(Poker_Tuple&);
 bool hasThree(Poker_Tuple&);
+bool hasDoubleTwo(Poker_Tuple&);
 bool hasTwo(Poker_Tuple&);
+
+bool handComparator(Poker_Tuple* t1, Poker_Tuple* t2);
+
+int nCharsOfA(char hand[5], char c);
